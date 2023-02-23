417a1ef261b8c6f9f49664360a18e0239531c22d
Michael Peter <pmic@zhaw.ch>
2023-02-23 10:19:36 +0100

    Removed motor examples for workshop 1

--- main.cpp
+++ main.cpp
@@ -16,14 +16,6 @@ void user_button_pressed_fcn(); // custom functions which get executed when user
 // main runs as an own thread
 int main()
 {
-    // states and actual state for state machine
-    const int ROBOT_STATE_INIT     = 0;
-    const int ROBOT_STATE_FORWARD  = 1;
-    const int ROBOT_STATE_BACKWARD = 2;
-    const int ROBOT_STATE_SLEEP    = 3;
-    int robot_state_actual = ROBOT_STATE_INIT;
-
-
     // attach button fall function to user button object, button has a pull-up resistor
     user_button.fall(&user_button_pressed_fcn);
 
@@ -46,45 +38,7 @@ int main()
 
     // Sharp GP2Y0A41SK0F, 4-40 cm IR Sensor
     float ir_distance_mV = 0.0f; // define variable to store measurement
-    AnalogIn ir_analog_in(PC_2); // create AnalogIn object to read in infrared distance sensor, 0...3.3V are mapped to 0...1
-
-
-    // Futaba Servo S3001 20mm 3kg Analog
-    Servo servo_S1(PB_2);     // create servo objects
-    Servo servo_S2(PC_8);
-    float servo_S1_angle = 0; // servo S1 normalized input: 0...1
-    float servo_S2_angle = 0;
-    
-    int servo_counter = 0;    // define servo counter, this is an additional variable to make the servos move
-    const int loops_per_seconds = static_cast<int>(ceilf( 1.0f / (0.001f * (float)main_task_period_ms) ));
-
-
-    // 78:1, 100:1, ... Metal Gearmotor 20Dx44L mm 12V CB
-    DigitalOut enable_motors(PB_15); // create DigitalOut object to enable dc motors
-
-    FastPWM pwm_M1(PB_13); // motor M1 is used open-loop
-    FastPWM pwm_M2(PA_9);  // motor M2 is closed-loop speed controlled (angle velocity)
-    FastPWM pwm_M3(PA_10); // motor M3 is closed-loop position controlled (angle controlled)
-
-    EncoderCounter  encoder_M1(PA_6, PC_7); // create encoder objects to read in the encoder counter values, since M1 is used open-loop no encoder would be needed for operation, this is just an example
-    EncoderCounter  encoder_M2(PB_6, PB_7);
-    EncoderCounter  encoder_M3(PA_0, PA_1);
-
-    // create SpeedController and PositionController objects, default parametrization is for 78.125:1 gear box
-    const float max_voltage = 12.0f;               // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
-    const float counts_per_turn = 20.0f * 78.125f; // define counts per turn at gearbox end: counts/turn * gearratio
-    const float kn = 180.0f / 12.0f;               // define motor constant in RPM/V
-    const float k_gear = 100.0f / 78.125f;         // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
-    const float kp = 0.1f;                         // define custom kp, this is the default speed controller gain for gear box 78.125:1
-
-    SpeedController speedController_M2(counts_per_turn, kn, max_voltage, pwm_M2, encoder_M2); // default 78.125:1 gear box  with default contoller parameters
-    // SpeedController speedController_M2(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M2, encoder_M2); // parameters adjusted to 100:1 gear
-
-    // PositionController positionController_M3(counts_per_turn, kn, max_voltage, pwm_M3, encoder_M3); // default 78.125:1 gear with default contoller parameters
-    PositionController positionController_M3(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M3, encoder_M3); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
-    positionController_M3.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
-    float max_speed_rps = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
-    positionController_M3.setMaxVelocityRPS(max_speed_rps); // adjust max velocity for internal speed controller
+    //??? // create AnalogIn object to read in infrared distance sensor, 0...3.3V are mapped to 0...1
 
 
     main_task_timer.start();
@@ -96,78 +50,15 @@ int main()
 
         if (do_execute_main_task) {
 
-            // read analog input
-            ir_distance_mV = 1.0e3f * ir_analog_in.read() * 3.3f;
+            if (mechanical_button.read()) {
+
+                // read analog input
+                //ir_distance_mV = ???;
+
+            }
 
             // visual feedback that the main task is executed, setting this once would actually be enough
             additional_led = 1;
-
-            // commanding the servos
-            if (servo_S1.isEnabled() && servo_S1.isEnabled()) {
-                
-                // command servo position, increment normalised angle every second until it reaches 1.0f
-                servo_S1.setNorlalisedAngle(servo_S1_angle);
-                if (servo_S1_angle < 1.0f & servo_counter%loops_per_seconds == 0 & servo_counter != 0) {
-                    servo_S1_angle += 0.01f;
-                }
-                servo_S2.setNorlalisedAngle(servo_S2_angle);
-                if (servo_S2_angle < 1.0f & servo_counter%loops_per_seconds == 0 & servo_counter != 0) {
-                    servo_S2_angle += 0.01f;
-                }
-                servo_counter++;
-            }
-
-            // state machine
-            switch (robot_state_actual) {
-
-                case ROBOT_STATE_INIT:
-
-                    // check if servos are enabled, should be alreay disabled at this point, it's just an example
-                    if (!servo_S1.isEnabled()) servo_S1.enable();
-                    if (!servo_S2.isEnabled()) servo_S2.enable();
-
-                    enable_motors = 1; // enable hardwaredriver dc motors: 0 -> disabled, 1 -> enabled
-
-                    robot_state_actual = ROBOT_STATE_FORWARD;
-                    break;
-
-                case ROBOT_STATE_FORWARD:
-
-                    if (mechanical_button.read()) {
-                        pwm_M1.write(0.75f); // write output voltage to motor M1
-                        speedController_M2.setDesiredSpeedRPS(0.5f); // set a desired speed for speed controlled dc motors M2
-                        positionController_M3.setDesiredRotation(1.5f); // set a desired rotation for position controlled dc motors M3
-
-                        robot_state_actual = ROBOT_STATE_BACKWARD;
-                    }
-                    break;
-
-                case ROBOT_STATE_BACKWARD:
-
-                    if (positionController_M3.getRotation() >= 1.45f) {
-                        pwm_M1.write(0.25f);
-                        speedController_M2.setDesiredSpeedRPS(-0.5f);
-                        positionController_M3.setDesiredRotation(0.0f);
-
-                        robot_state_actual = ROBOT_STATE_SLEEP;
-                    }
-                    break;
-
-                case ROBOT_STATE_SLEEP:
-
-                    if (positionController_M3.getRotation() <= 0.05f) {
-                        enable_motors = 0;
-                        pwm_M1.write(0.5f);
-                        speedController_M2.setDesiredSpeedRPS(0.5f);
-                        
-                        // robot_state_actual is not changed, there for the state machine remains in here until the blue button is pressed again
-                    }
-
-                default:
-                
-                    // do nothing
-                    break;
-            }
 
         } else {
 
@@ -175,16 +66,6 @@ int main()
                 do_reset_all_once = false;
 
                 ir_distance_mV = 0.0f;
-
-                pwm_M1.write(0.5f);
-                speedController_M2.setDesiredSpeedRPS(0.0f);
-                positionController_M3.setDesiredRotation(0.0f);
-                robot_state_actual = ROBOT_STATE_INIT;
-
-                servo_S1_angle = 0;
-                servo_S2_angle = 0;
-                servo_S1.disable();
-                servo_S2.disable();
 
                 additional_led = 0;
             }            
@@ -194,13 +75,7 @@ int main()
         user_led = !user_led;
 
         // do only output via serial what's really necessary, this makes your code slow
-        printf("IR sensor (mV): %3.3f, Encoder M1: %3d, Speed M2 (rps) %3.3f, Position M3 (rot): %3.3f, Servo S1 angle (normalized): %3.3f, Servo S2 angle (normalized): %3.3f\r\n",
-               ir_distance_mV,
-               encoder_M1.read(),
-               speedController_M2.getSpeedRPS(),
-               positionController_M3.getRotation(),
-               servo_S1_angle,
-               servo_S2_angle);
+        printf("IR sensor (mV): %3.3f\r\n", ir_distance_mV);
 
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
