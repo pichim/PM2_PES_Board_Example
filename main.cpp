d92fc156342305b41e0b0963e6949d3c5c0f58ae
arya <aryapen1@students.zhaw.ch>
2023-04-11 12:05:47 +0200

    major changes in switch case

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iterator>
 #include <mbed.h>
 
 #include "PM2_Drivers.h"
@@ -11,7 +12,19 @@ bool do_reset_all_once = false;    // this variable is used to reset certain var
 // objects for user button (blue button) handling on nucleo board
 DebounceIn user_button(PC_13);  // create InterruptIn interface object to evaluate user button falling and rising edge (no blocking code in ISR)
 void user_button_pressed_fcn(); // custom functions which get executed when user button gets pressed, definition below
-
+float total_distance_M2 = 0.0f;
+float total_distance_M3 = 0.0f;
+float calculate_distance_traveled(float rotations, float wheel_diameter)
+{
+     float wheel_radius_cm = wheel_diameter / 2.0f;
+    float wheel_circumference_cm = 2.0f * M_PI * wheel_radius_cm;
+    float distance = rotations * wheel_diameter * M_PI;
+    float total_distance_M2 =+ distance * 10.0f;
+    float total_distance_M3 =+ distance * 10.0f;
+     printf("Distance 1M3: %f\n", total_distance_M2);
+     printf("Distance 1M2: %f\n", total_distance_M3);
+    return  total_distance_M3;
+}
 
 // main runs as an own thread
 int main()
@@ -27,11 +40,8 @@ int main()
     const int ROBOT_STATE_SET_POSITION        = 6;
     const int ROBOT_STATE_REACH_GOAL          = 7;
     
-   
-    
+
     int robot_state_actual = ROBOT_STATE_INIT;
-
-
     // attach button fall function to user button object, button has a pull-up resistor
     
 
@@ -63,21 +73,27 @@ int main()
     EncoderCounter  encoder_M1(PA_6, PC_7); // create encoder objects to read in the encoder counter values, since M1 is used open-loop no encoder would be needed for operation, this is just an example
     EncoderCounter  encoder_M2(PB_6, PB_7);
     EncoderCounter  encoder_M3(PA_0, PA_1);
-
+   
+   
     // create SpeedController and PositionController objects, default parametrization is for 78.125:1 gear box
     const float max_voltage = 12.0f;               // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
     const float counts_per_turn = 20.0f * 78.125f; // define counts per turn at gearbox end: counts/turn * gearratio
     const float kn = 180.0f / 12.0f;               // define motor constant in RPM/V
     const float k_gear = 100.0f / 78.125f;         // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
     const float kp = 0.1f;                         // define custom kp, this is the default speed controller gain for gear box 78.125:1
-
+          float distance = 0.0f;
+          float turn_for_1cm = 0.106f;  // entspricht 1 cm
+    const float wheel_diameter = 0.3f;   
+          float total_distance_M3 = 0.0f;
+          float total_distance_M2 = 0.0f; 
+          
     PositionController positionController_M1(counts_per_turn, kn, max_voltage, pwm_M2, encoder_M1);
     positionController_M1.setSpeedCntrlGain(kp * k_gear);
     float max_speed_rps1 = 0.5f;
     positionController_M1.setMaxVelocityRPS(max_speed_rps1);
 
-     PositionController positionController_M2(counts_per_turn, kn, max_voltage, pwm_M2, encoder_M2); // default 78.125:1 gear box  with default contoller parameters
-     positionController_M2.setSpeedCntrlGain(kp * k_gear);
+    PositionController positionController_M2(counts_per_turn, kn, max_voltage, pwm_M2, encoder_M2); // default 78.125:1 gear box  with default contoller parameters
+    positionController_M2.setSpeedCntrlGain(kp * k_gear);
     float max_speed_rps2 = 0.5f;
     positionController_M2.setMaxVelocityRPS(max_speed_rps2);
     // SpeedController speedController_M2(counts_per_turn * k_gear, kn / k_gear, max_voltage, pwm_M2, encoder_M2); // parameters adjusted to 100:1 gear
@@ -89,6 +105,10 @@ int main()
     positionController_M3.setSpeedCntrlGain(kp * k_gear);   // adjust internal speed controller gain, this is just an example
     float max_speed_rps3 = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
     positionController_M3.setMaxVelocityRPS(max_speed_rps3); // adjust max velocity for internal speed controller
+ float rotations_M3 = positionController_M3.getRotation();
+                    float distance_M3 = calculate_distance_traveled(rotations_M3, wheel_diameter); 
+                        total_distance_M3 += distance_M3;
+                          printf("Distance MM: %f\n", total_distance_M3);
 
 
     main_task_timer.start();
@@ -124,36 +144,43 @@ int main()
                 case ROBOT_STATE_FORWARD: // robot moves forward when the motors are enabled
 
                     if (mechanical_button.read()) {
+                        positionController_M3.setDesiredRotation(3.19f); //bewegt sich bis zum hindernis
+                        rotations_M3 = positionController_M3.getRotation();
+                        distance_M3 = calculate_distance_traveled(rotations_M3, wheel_diameter); 
+                        total_distance_M3 += distance_M3;
+                        positionController_M2.setDesiredRotation(3.19f); // set a desired speed for speed controlled dc motors M2
+               
 
-                        positionController_M1.setDesiredRotation(10.0f); 
-                        positionController_M3.setDesiredRotation(10.0f); //bewegt sich bis zum hindernis
-                        positionController_M2.setDesiredRotation(10.0f); // set a desired speed for speed controlled dc motors M2
-                        robot_state_actual = ROBOT_STATE_BACKWARD;
                     }
                     break;
+                   
 
                 case ROBOT_STATE_BACKWARD:  // not sure of needed in this code //evtl. brauchen wir es nicht
 
-                    if (positionController_M1.getRotation() >= 1.45f) {
+                    if ( positionController_M3.getRotation() >= 3.19f ) {
+                        
 
-                        pwm_M1.write(0.25f);
-
-                        positionController_M1.setDesiredRotation(-0.5f);
+                        positionController_M3.setDesiredRotation(-5.5f);
+                        positionController_M1.setDesiredRotation(-5.5f);
+                          float rotations_M3 = positionController_M3.getRotation();
+                    float distance_M3 = calculate_distance_traveled(rotations_M3, wheel_diameter); 
+                    total_distance_M3 += distance_M3;
                         positionController_M2.setDesiredRotation(-0.5f);
-                        positionController_M3.setDesiredRotation(0.0f);
 
-                        robot_state_actual = ROBOT_STATE_SLEEP;
+                        robot_state_actual = ROBOT_STATE_ARM_SETS_Angle;
                     }
                     break;
 
                 case ROBOT_STATE_SLEEP:  // robot sleeps when the desired position in rad is achieved
 
-                    if (positionController_M2.getRotation() <= 0.05f && positionController_M2.getRotation() <= 0.05f) { //wenn M1 und M2 die gewünshte rotation bis zum hindernis erreicht hat
+                    if (positionController_M3.getRotation()>= 0.0f) { //wenn M1 und M2 die gewünshte rotation bis zum hindernis erreicht hat
                         enable_motors = 0; // motor wird ausgeschaltet
-                        pwm_M1.write(0.5f);
 
-                        positionController_M1.setDesiredRotation(0.5f);
-                       positionController_M2.setDesiredRotation(0.5f);
+                       positionController_M2.setDesiredRotation(0.0f);
+                       positionController_M3.setDesiredRotation(0.0f);
+                         float rotations_M3 = positionController_M3.getRotation();
+                    float distance_M3 = calculate_distance_traveled(rotations_M3, wheel_diameter); 
+                    total_distance_M3 += distance_M3;
                        robot_state_actual = ROBOT_STATE_ARM_SETS_Angle;
                         
                         // robot_state_actual is not changed, there for the state machine remains in here until the blue button is pressed again
@@ -161,37 +188,38 @@ int main()
                     break;
                 
                 case ROBOT_STATE_ARM_SETS_Angle: // arm sets angle when the desired Position in Rad from case ROBOT_SLEEP is achieved 
-                if (positionController_M1.getRotation() <= 0.05f && positionController_M2.getRotation() <= 0.05f) {
-                    enable_motors = 1;
-                positionController_M3.setDesiredRotation(1.5f); // set a desired rotation for position controlled dc motors M3
+                if (positionController_M3.getRotation() <= 3.13f) {
+                  
+                positionController_M3.setDesiredRotation(3.0f); // set a desired rotation for position controlled dc motors M3
+                  float rotations_M3 = positionController_M3.getRotation();
+                    float distance_M3 = calculate_distance_traveled(rotations_M3, wheel_diameter); 
+                    total_distance_M3 += distance_M3;
                 robot_state_actual = ROBOT_STATE_MOVE_ARM_AND_WHEELS;
                 }
                     break;
 
                 case ROBOT_STATE_MOVE_ARM_AND_WHEELS: //moves forward while moving arm
-                if (positionController_M3.getRotation() <= 0.05f){
-                    pwm_M1.write(0.5f); // pwm regelt die geschwindigkeit um einen prozentteil
-                    pwm_M2.write(0.5f); // für den arm und die raupen kann man verschiedene % nehmen um snychron das hindernis zu überwältigen
-                    pwm_M3.write(0.5f); 
-                    positionController_M1.setDesiredRotation(1.5f);
-                    positionController_M2.setDesiredRotation(1.5f);
-                    positionController_M3.setDesiredRotation(1.5f);
+                if (positionController_M3.getRotation() <= 6.16f){
+                    
+                    positionController_M1.setDesiredRotation(5.5f);
+                    positionController_M2.setDesiredRotation(5.5f);
+                    positionController_M3.setDesiredRotation(5.5f);
                     robot_state_actual = ROBOT_STATE_SET_POSITION;
                 }
                     break;
 
                 case ROBOT_STATE_SET_POSITION: //sets its position after going above hurdle
                 if (positionController_M3.getRotation() <= 0.05f){ // wenn der arm die endposition erreicht (umdrehung bis zum anschlag)
-                    positionController_M1.setDesiredRotation(10.0f); //muss sich nach hinten bewegen bis zur Hürde
-                    positionController_M2.setDesiredRotation(10.0f); // gleich wie M1
+                    positionController_M1.setDesiredRotation(0.02f); //muss sich nach hinten bewegen bis zur Hürde
+                    positionController_M2.setDesiredRotation(0.02f); // gleich wie M1
                     robot_state_actual = ROBOT_STATE_REACH_GOAL;     // die reichweite müsste man testen
                 }
                     break;
 
                 case ROBOT_STATE_REACH_GOAL:
-                if (positionController_M1.getRotation() <= 0.05f && positionController_M2.getRotation() <= 0.05f){ //wenn die gewünschte position im ROBOT_SET erreicht wurde
-                    positionController_M1.setDesiredRotation(10.0f); // rotation bis zum endziel
-                    positionController_M2.setDesiredRotation(10.0f); // set a desired speed for speed controlled dc motors M2
+                if (positionController_M1.getRotation() <= 0.05f ){ //wenn die gewünschte position im ROBOT_SET erreicht wurde
+                    positionController_M1.setDesiredRotation(3.18f); // rotation bis zum endziel
+                    positionController_M2.setDesiredRotation(3.18f); // set a desired speed for speed controlled dc motors M2
                     enable_motors = 0;
                     robot_state_actual = ROBOT_STATE_INIT;
                 }
@@ -218,8 +246,11 @@ int main()
 
 
                 additional_led = 0;
-            }            
+            }   
+            
+
         }
+        
 
         // toggling the user led
         user_led = !user_led;
@@ -230,8 +261,15 @@ int main()
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
         thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
+        
     }
+
+
+
+    
 }
+
+
 
 void user_button_pressed_fcn()
 {
